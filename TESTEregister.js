9c98cdf705b56ecda6295dd20940d2152e671c4f
DMoriant <120055089+DMoriant@users.noreply.github.com>
2023-08-17 06:22:04 -0300

    tentativa de criação de sistema de cadastro. Bugs ocorrendo Páginas com "TESTE" para serem analisadas/ajustadas/deletadas Instação de node.js, express, mongodb, bcrypt, mongoose, react router dom

---
+++ TESTEregister.js
@@ -0,0 +1,119 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const registrationForm = document.getElementById('registration-form');
+
+    registrationForm.addEventListener('submit', async function (event) {
+        event.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        const email = document.getElementById('email').value;
+
+        const userData = { username, password, email };
+
+        try {
+            const response = await fetch('/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            });
+
+            const responseData = await response.json();
+
+            if (response.ok) {
+                alert(responseData.message);
+                // Redirecionar para outra página após o registro bem-sucedido
+                window.location.href = '/login.html';
+            } else {
+                alert(responseData.message);
+            }
+        } catch (error) {
+            console.error('Erro ao registrar usuário:', error);
+        }
+    });
+});
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const url = "mongodb+srv://pablovieira04:6Ki9sv1gNyjpVVyz@cluster0.vgygmui.mongodb.net/"; // Substitua pela URL de conexão fornecida pelo MongoDB Atlas
+const dbName = 'users';
+const collectionName = 'users';
+
+app.post('/register', async (req, res) => {
+    const { username, password, email } = req.body;
+
+    if (!username || !password || !email) {
+        return res.status(400).json({ message: 'Todos os campos são obrigatórios.' });
+    }
+
+    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+
+        // Verificar se o usuário já existe
+        const existingUser = await collection.findOne({ username });
+        if (existingUser) {
+            return res.status(409).json({ message: 'Nome de usuário já está em uso.' });
+        }
+
+        const newUser = {
+            username,
+            password,
+            email
+        };
+
+        await collection.insertOne(newUser);
+        res.status(201).json({ message: 'Usuário registrado com sucesso.' });
+    } catch (err) {
+        console.error('Erro ao registrar usuário:', err);
+        res.status(500).json({ message: 'Erro interno do servidor.' });
+    } finally {
+        client.close();
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
+async function registerUser() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+
+    const userData = { username, password, email };
+
+    try {
+        const response = await fetch('/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+
+        const responseData = await response.json();
+
+        if (response.ok) {
+            alert(responseData.message);
+            // Redirecionar para outra página após o registro bem-sucedido
+            window.location.href = '/login.html';
+        } else {
+            alert(responseData.message);
+        }
+    } catch (error) {
+        console.error('Erro ao registrar usuário:', error);
+    }
+}
